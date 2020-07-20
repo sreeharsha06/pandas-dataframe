8e21826dd73a10237bdc7a3bb505c237d360ec7a
sreeharsha06 <harshabellamkonda46@gmail.com>
2020-07-21 05:01:34 +0530

    Created a Dataframes with pandas

---
+++ dataframe.py
@@ -0,0 +1,4 @@
+import pandas as pd
+family_info={'NAME':['Sharath','Sreeharsha','Raghavi','Srihitha'],'Year_of_Birth':[1993,2000,1998,2003],'Date_of_Birth':[25,6,10,25]}
+family=pd.DataFrame(family_info)
+print(family)
